30ad2edacdfaf114b308b5dc7314e69b1d9d2423
Ripul19 <ripulkhurana77@gmail.com>
2023-04-19 20:48:27 +0530

    first commit

--- routes.js
+++ routes.js
@@ -1,17 +1,4 @@
-const http = require('http');
-const fs = require('fs');
-
-//ways to write createserver
-//function rqListener(req , res){}  
-// http.createServer(rqListener);
-//http.createServer(function (req, res){});
-
-const server = http.createServer((req, res) => {
-    //console.log(req.url, req.method, req.headers);
-   // process.exit(2000);
-   const url = req.url;
-   const method = req.method;
-   if(url ==='/'){
+if(url ==='/'){
     res.write('<html>');
    res.write('<head><title>Enter Message</title></head>');
    res.write('<body><form action="/message" method = "POST"><input type = "text" name = "message"><button type = "submit">Send</button></form></body>');
@@ -24,14 +11,14 @@ if (url === '/message' && method === 'POST'){
         console.log(chunk);
         body.push(chunk);
     });
-
-   return req.on('end', () => {
+    
+    return req.on('end', () => {
         const parsedBody = Buffer.concat(body).toString();
         const message = parsedBody.split('=')[1];
         fs.writeFile ('message.txt', message, (err) => {
             res.statusCode = 302;
-    res.setHeader('Location','/');
-    return res.end();
+            res.setHeader('Location','/');
+            return res.end();
         }); 
     });
 
@@ -47,9 +34,3 @@ if (url === '/message' && method === 'POST'){
    res.write('<body><h1>Hello from Node.js server!</h1></body>');
    res.write('</html>');
    res.end(); 
-});
-
-server.listen(3000);
-
-//after running the upper code the program was never closed because of "Event Listeners"
-//Program keeps on running as long as there are event listeners registered
