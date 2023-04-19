00b6001a61a0cb8a9e12544071f2c1225beed59f
Ripul19 <ripulkhurana77@gmail.com>
2023-04-20 02:44:51 +0530

    understanding the basics part complete

--- routes.js
+++ routes.js
@@ -1,55 +1,56 @@
-const requestHandler = (req,res) =>{
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
     const url = req.url;
     const method = req.method;
+if(url ==='/'){
+    res.write('<html>');
+   res.write('<head><title>Enter Message</title></head>');
+   res.write('<body><form action="/message" method = "POST"><input type = "text" name = "message"><button type = "submit">Send</button></form></body>');
+   res.write('</html>');
+   return res.end(); 
+   }
+if (url === '/message' && method === 'POST'){
+    const body = []
+    req.on('data', (chunk) => {
+        console.log(chunk);
+        body.push(chunk);
+    });
+    
+    return req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split('=')[1];
+        fs.writeFile ('message.txt', message, (err) => {
+            res.statusCode = 302;
+            res.setHeader('Location','/');
+            return res.end();
+        }); 
+    });
 
-    if (url === '/'){
-        res.setHeader('Content-Type','text/html');
-        res.write('<html>');
-        res.write('<head><title>Greetings</title></head>');
-        res.write('<h1>Welcome to my website</h1>');
-        res.write('<h2>Please enter username</h2>');
-        res.write('<body><form action = "/create-user" method = "POST"><label>Firstname</label><br><input type ="text" name = "fname"><br><Button type = "submit">Submit</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if (url === '/users'){
-        res.setHeader('Content-Type','text/html');
-        res.write('<html>');
-        res.write('<head><title>Users List</title></head>');
-        res.write('<h1>List of the users</h1>');
-        res.write('<body><ul><li>User1</li><li>User2</li><li>User3</li></ul></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if (url !== '/create-user'){
-        res.setHeader('Content-Type','text/html');
-        res.write('<html>');
-        res.write('<head><title>PAGE NOT FOUND</title></head>');
-        res.write('<h1>PAGE NOT FOUND !!!</h1>');
-        res.write('</html>');
-        return res.end();
-    }
-
-
-    if (url === '/create-user' && method === 'POST'){
-        const username = []
-        req.on('data', (chunk) => {
-            username.push(chunk);
-        });
-
-        req.on('end', () => {
-            const parsedUsername = Buffer.concat(username).toString();
-            const finalUsername = parsedUsername.split('=')[1];
-            console.log(finalUsername);
-        });
-        res.statusCode = 302;
-        res.setHeader('Location','/');
-        return res.end();
-    }
-
+   /* res.statusCode = 302;
+    res.setHeader('Location','/');
+    return res.end();*/
+    
 }
 
+   res.setHeader('Content-Type', 'text/html');
+   res.write('<html>');
+   res.write('<head><title>My First Page</title></head>');
+   res.write('<body><h1>Hello from Node.js server!</h1></body>');
+   res.write('</html>');
+   res.end(); 
+};
 
-module.exports = requestHandler;
+
+//module.exports= requestHandler;
+
+exports.handler = requestHandler;
+exports.someText = 'Some Hard Coded Text';
+
+/* module.exports = {
+    handler: requestHandler,
+    someText: 'Some hard coded text'
+};*/
+/* module.exports.andler = requestHandler;
+module.exports.someText = 'Some Hard Coded Text';
+ */
