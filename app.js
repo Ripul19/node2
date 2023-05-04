e97af99279a51653653d048dd66c6d86622dd24d
Ripul19 <ripulkhurana77@gmail.com>
2023-05-04 18:47:13 +0530

    ejs pug handlebars templates complete

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const path = require('path');
 const express = require('express');
+// const expressHbs = require('express-handlebars');
 const app = express();
+
+//used for pug 
+// app.set('view engine', 'pug');
+
+//used when not using default layout for handlebars
+// app.engine('hbs', expressHbs.engine({extname: "hbs", defaultLayout: false, layoutsDir: "views/layouts/"}));
+
+//default layout for handlebars
+// app.engine('hbs', expressHbs.engine({extname: "hbs", defaultLayout: "main-layout.hbs", layoutsDir: "views/layouts/"}));
+// app.set('view engine', 'hbs');
+
+//for ejs
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -11,7 +26,17 @@ app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req,res,next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views','pageNotFound.html'));
+    //used for general html files
+    // res.status(404).sendFile(path.join(__dirname, 'views','pageNotFound.html'));
+    
+    //used for pug files
+    // res.status(404).render('pageNotFound', {pageTitle: 'Page Not Found'});
+
+    //used for handlebars
+    //res.status(404).render('pageNotFound', {pageTitle: 'Page Not Found'});
+
+    //for ejs
+    res.status(404).render('pageNotFound', {pageTitle: 'Page Not Found', path: ''});
 });
 
 app.listen(3000);
