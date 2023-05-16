7b0d48698e7ea01d32b8d1ce613df4b49b634c10
Ripul19 <ripulkhurana77@gmail.com>
2023-05-16 18:30:37 +0530

    started with Mysql database

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 const ErrorsController = require('./controllers/error');
+const db = ('./util/database');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELEXT 8 from Products').then().catch();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Khurana1222@'
+});
+
+module.exports = pool.promise();
